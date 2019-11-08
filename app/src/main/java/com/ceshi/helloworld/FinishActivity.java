45dda124dce4296350575d8dcd54780fecd899b6
HuangZhipeng996 <727181639@qq.com>
2019-11-08 16:27:33 +0800

    hzp

--- app/src/main/java/com/ceshi/helloworld/FinishActivity.java
+++ app/src/main/java/com/ceshi/helloworld/FinishActivity.java
@@ -2,6 +2,8 @@ package com.ceshi.helloworld;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,9 +12,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.ceshi.helloworld.net.CommonData;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class FinishActivity extends AppCompatActivity {
 
+public class FinishActivity extends AppCompatActivity  {
+    private TextView time;
+    private  int i=0;
+    private Timer timer=null;
+    private TimerTask task=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +37,9 @@ public class FinishActivity extends AppCompatActivity {
 
         //倒计时30s立即返回到 首界面
 
-
+        time=(TextView) findViewById(R.id.tv_time);
+        i=Integer.parseInt(time.getText().toString());
+        startTime();
 
 
 
@@ -53,5 +63,28 @@ public class FinishActivity extends AppCompatActivity {
 
     }
 
+    public void startTime() {
+        timer = new Timer();
+        task = new TimerTask() {
 
+            @Override
+            public void run() {
+                if (i > 0) {   //加入判断不能小于0
+                    i--;
+                    Message message = mHandler.obtainMessage();
+                    message.arg1 = i;
+                    mHandler.sendMessage(message);
+                }else {
+
+                }
+            }
+        };
+        timer.schedule(task, 1000);
+    }
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            time.setText(msg.arg1 + "");
+            startTime();
+        };
+    };
 }
